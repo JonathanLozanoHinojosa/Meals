b735aef9cc3b45062be82cc4062cc02948652350
JonathanLozanoHinojosa <jona.lh.89@gmail.com>
2024-06-20 11:08:06 +0200

    getting data from model

--- js/favourites.js
+++ js/favourites.js
@@ -1,5 +1,7 @@
+import { getAllData } from "../module/getData.js";
+
+// Objectes del DOM
 const section = document.querySelector('section');
-const spinner = document.querySelector("#spinner");
 const busqueda = document.querySelector("#buscar");
 
 let arrayFavoritos = [];
@@ -7,18 +9,12 @@ let arrayFavoritos = [];
 let recetasBackup = [];
 const arrayFiltrado = {};
 
+// Carrega inicial de l'aplicació
 const recetas = await getAllData();
+obtenerFavoritos(recetas);
+mostrarDatosFavoritos(arrayFiltrado);
 
-/************************** SPINNER **************************/
-function showSpinner() {
-    spinner.classList.add("display");
-}
-
-function hideSpinner() {
-    spinner.classList.remove("display");
-}
-
-/************************** BUSQUEDA **************************/
+/************************** CERCA **************************/
 function funcionFiltro(arrayFiltrado) {
     const palabra = document.querySelector("#palabraABuscar").value;
     return arrayFiltrado.strMeal.toLowerCase().includes(palabra.toLowerCase());
@@ -31,36 +27,12 @@ busqueda.addEventListener('click', function (e) {
     mostrarDatosFavoritos(recetasObjeto);
 });
 
-
-/************************** OBTENCIÓ DE RECEPTES API **************************/
-async function getAllData() {
-    try {
-        showSpinner();
-
-        // Accedeix a la API i recupera dades
-        const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?f=%";
-        const response = await fetch(apiUrl); 
-        const recetas = await response.json();
-        //console.log(recetas.meals[0]);
-        //return recetas;
-        
-        hideSpinner();
-        obtenerFavoritos(recetas);
-        //console.log(arrayFavoritos)
-        mostrarDatosFavoritos(arrayFiltrado);
-        return recetas
-    } catch(error){
-        console.log("Error fetching data ", error);
-    }
-    //container.textContent = JSON.stringify(data);
-}
-
-/************************** MOSTRAR QUANTITAT RECEPTES  **************************/
+/************************** MOSTRAR QUANTITAT RECEPTES **************************/
 function mostrarNumeroRecetas(recetas) {
     document.querySelector('.numItems').textContent = `Meals: ${recetas.meals.length} items`;
 }
 
-// MOSTRAR SOLO FAVORITOS
+// De totes les receptes, mostrem nomès les receptes que estiguin al localstorage
 function obtenerFavoritos(recetas) {
     const storedUserData = localStorage.getItem('favouriteMeals');
        
@@ -79,32 +51,25 @@ function obtenerFavoritos(recetas) {
 
 }
 
-/************************** MOSTRAR DADES  **************************/
+/************************** MOSTRAR DADES AL DOM  **************************/
 function mostrarDatosFavoritos(recetas) {
     
     clearDiv();
     const div = document.querySelector('#recetas').content;
     
-    /***** ARRAY DE ELEMENTOS SIN IMAGEN PREVIEW *************/
+    // Array dels elements sense imatge preview
     const idSinPreview = ['52873', '52900', '52930', '52932'];
 
-    //console.log(recetas)
     for (const receta of recetas.meals) {
-        //const resultado = arrayFavoritos.find((recipe) => recipe.name ===  receta.strMeal);
-        
-        //if (resultado) {
-            //recetasBackup.push(receta);
-            const fr = div.cloneNode(true);
+        const fr = div.cloneNode(true);
            
-            idSinPreview.includes(receta.idMeal) ? fr.querySelector("img").src = receta.strMealThumb : fr.querySelector("img").src = receta.strMealThumb+"/preview";
-            fr.querySelector("img").alt= receta.strMeal;
-            fr.querySelector(".title").textContent = receta.strMeal;
-            fr.querySelector("a").href = 'detail.html?s='+receta.idMeal;
-            section.appendChild(fr); // Mostrar en el DOM*/
-       // }
+        idSinPreview.includes(receta.idMeal) ? fr.querySelector("img").src = receta.strMealThumb : fr.querySelector("img").src = receta.strMealThumb+"/preview";
+        fr.querySelector("img").alt= receta.strMeal;
+        fr.querySelector(".title").textContent = receta.strMeal;
+        fr.querySelector("a").href = 'detail.html?s='+receta.idMeal;
+        section.appendChild(fr); // Mostrar en el DOM*/
     }
-    //arrayFiltrado["meals"] = recetasBackup;
-    //console.log(arrayFiltrado)
+    
     mostrarNumeroRecetas(recetas);
 }
 
