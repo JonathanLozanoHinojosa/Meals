0d08efb60d4d220d5d3437dd358628838e144772
JonathanLozanoHinojosa <jona.lh.89@gmail.com>
2024-06-20 11:08:43 +0200

    getting data from model

--- js/recipes.js
+++ js/recipes.js
@@ -1,23 +1,22 @@
+import {getAllData, getCategories} from '../module/getData.js';
+
+// Objectes del DOM
 const section = document.querySelector('section');
-const spinner = document.querySelector("#spinner");
 const busqueda = document.querySelector("#buscar");
 const contenedorDropdown = document.querySelector('div.dropdown');
 const botonCategoriaFiltro = document.querySelector("#botonFilterCategory");
 
-let recetasBackup = [];
+// Carrega inicial de l'aplicació
 let categoriasSeleccionadas = [];
 const recetas = await getAllData();
+let recetasBackup = recetas;
+let categorias = [];
+categorias = await getCategories(categorias);
+mostrarDatos(recetas);
+addCategoriesDropdown(categorias);
 
-/************************** SPINNER **************************/
-function showSpinner() {
-    spinner.classList.add("display");
-}
 
-function hideSpinner() {
-    spinner.classList.remove("display");
-}
-
-/************************** BUSQUEDA **************************/
+/************************** CERCA PER NOM **************************/
 function funcionFiltro(recetas) {
     const palabra = document.querySelector("#palabraABuscar").value;
     return recetas.strMeal.toLowerCase().includes(palabra.toLowerCase());
@@ -31,7 +30,7 @@ busqueda.addEventListener('click', function (e) {
     mostrarDatos(recetasObjeto)
 });
 
-/************************** ORDENACIÓ **************************/
+/************************** ORDENACIÓ A-Z/Z-A **************************/
 const select = document.getElementById('select');
 
 select.addEventListener('change', function (e) {
@@ -45,53 +44,28 @@ select.addEventListener('change', function (e) {
             return (b.strMeal.trimStart().localeCompare(a.strMeal.trimStart()));
           });
     }
+    
     mostrarDatos(recetasBackup);
 });
 
-/************************** OBTENCIÓ DE RECEPTES API **************************/
-async function getAllData() {
-    try {
-        showSpinner();
-
-        // Accedeix a la API i recupera dades
-        const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?f=%";
-        const response = await fetch(apiUrl); 
-        const recetas = await response.json();
-        //console.log(recetas.meals[0]);
-        //return recetas;
-
-        recetasBackup = recetas;
-        let categorias = [];
-
-        hideSpinner();
-        categorias = await getCategories(categorias);
-        mostrarDatos(recetas);
-        addCategoriesDropdown(categorias);
-        return recetas;
-    } catch(error){
-        console.log("Error fetching data ", error);
-    }
-    //container.textContent = JSON.stringify(data);
-}
-
-/************************** MOSTRAR QUANTITAT RECEPTES  **************************/
+/************************** MOSTRAR QUANTITAT DE RECEPTES  **************************/
 function mostrarNumeroRecetas(recetas) {
     document.querySelector('.numItems').textContent = `Meals: ${recetas.meals.length} items`;
 }
 
 
-/************************** MOSTRAR DADES  **************************/
+/************************** MOSTRAR DADES DOM **************************/
 function mostrarDatos(recetas) {
     
     clearDiv();
     const div = document.querySelector('#recetas').content;
     
-    /**** FRAGMENT **********/
+    // Creació del fragment
     const fragment = document.createDocumentFragment();
     
-    /***** ARRAY DE ELEMENTOS SIN IMAGEN PREVIEW *************/
+    // Array ambs id's de les receptes sense imatge preview
     const idSinPreview = ['52873', '52900', '52930', '52932'];
-    //console.log(recetas)
+    
     for (const receta of recetas.meals) {
         const fr = div.cloneNode(true);
         
@@ -101,10 +75,11 @@ function mostrarDatos(recetas) {
         fr.querySelector("a").href = 'detail.html?s='+receta.idMeal;
         
         //section.appendChild(fr); // Mostrar en el DOM*/
-        /* FRAGMENT */
+        // Afegim al fragment
         fragment.appendChild(fr);
     }
-    /* AÑADIR EL FRAGMENT AL CONTENEDOR */
+
+    // Afegim el fragment al contenidor
     section.appendChild(fragment);
     mostrarNumeroRecetas(recetas);
 }
@@ -115,30 +90,11 @@ function clearDiv(div) {
 }
 
 
+/*************** CATEGORIES ***************/
 
-/************************ GET CATEGORIES FROM API ************************************/
-async function getCategories(categorias) {
-    try {
-        // Accedeix a la API i recupera dades
-        const apiUrl = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
-        const response = await fetch(apiUrl); 
-        const categories = await response.json();
-        
-        //console.log(data.meals[0].strCategory)
-        for (const category of categories.meals) {
-            categorias.push(category.strCategory);
-        }
-        return categorias;
-        //container.textContent = JSON.stringify(data);
-    } catch(error){
-        console.log("Error fetching data ", error);
-    }
-}
-
-/*************** SHOW CATEGORIES IN DROPDOWN LIST ***********************/
+// Mostrar les categories al dropdown list
 function addCategoriesDropdown (categorias) {
     for(const categoria of categorias){
-        //console.log(categoria);
 
         const div = document.querySelector('#dropdownTemplate').content;
         const contenedor = document.querySelector('.dropdownContent');
@@ -152,7 +108,7 @@ function addCategoriesDropdown (categorias) {
     }
 }
 
-/******************* EVENT CLICKING ELEMENTS *************************/
+// Event clicking elements in dropdown list
 contenedorDropdown.addEventListener('click', function(event) {
     const dropdown = document.querySelector('.dropdownContent');
     const inputs = document.querySelectorAll('input#a');
@@ -168,6 +124,7 @@ contenedorDropdown.addEventListener('click', function(event) {
     }
 });
 
+// Event al fer click al botó de categories
 botonCategoriaFiltro.addEventListener('click', function() {
     let recetasFilter = recetas.meals.filter(filtroCategoria);
     let recetasObjeto = {};
@@ -175,9 +132,8 @@ botonCategoriaFiltro.addEventListener('click', function() {
     mostrarDatos(recetasObjeto)
 });
 
+// Filtre per mostrar nomès les receptes seleccionades
 function filtroCategoria(recetas) {
-    //console.log(categoriasSeleccionadas.includes(recetas.strCategory));
-    //console.log(recetas.strCategory);
     if (categoriasSeleccionadas.length === 0) {
         return recetas;
     } else {
